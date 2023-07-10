6c194d234d546708dd9826104b3bb088333e07de
Dmitree <color.ivanov2016@yandex.ru>
2023-07-10 22:08:54 +0300

    init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# java-telros-app
---
+++ src/test/java/dmitreev/testwork/java/telrosapp/TelrosAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dmitreev.testwork.java.telrosapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TelrosAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
